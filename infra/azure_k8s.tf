d3b56760f1bc2695a817bfc2cf7d55733cd27365
Chris Zembower <chris.zembower@hashicorp.com>
2023-06-13 17:06:20 -0400

    aks basics

---
+++ infra/azure_k8s.tf
@@ -0,0 +1,75 @@
+data "azurerm_role_definition" "contributor" {
+  name = "Contributor"
+}
+
+data "azurerm_subscription" "this" {}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  name                = "uaid-aks-${random_id.this.hex}"
+}
+
+resource "azurerm_role_assignment" "aks" {
+  scope              = data.azurerm_subscription.this.id
+  role_definition_id = data.azurerm_role_definition.contributor.id
+  principal_id       = azurerm_user_assigned_identity.aks.principal_id
+
+  skip_service_principal_aad_check = true
+
+  lifecycle {
+    ignore_changes = [
+      role_definition_id
+    ]
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "this" {
+  depends_on = [
+    azurerm_subnet_nat_gateway_association.private,
+    azurerm_nat_gateway_public_ip_association.this
+  ]
+
+  name                              = "aks-rsa-${random_id.this.hex}"
+  location                          = azurerm_resource_group.this.location
+  resource_group_name               = azurerm_resource_group.this.name
+  kubernetes_version                = null // latest stable
+  dns_prefix                        = "aks"
+  role_based_access_control_enabled = true
+  private_cluster_enabled           = true
+  private_dns_zone_id               = "System"
+  automatic_channel_upgrade         = "rapid"
+
+  default_node_pool {
+    name                   = "default"
+    enable_auto_scaling    = true
+    min_count              = 3
+    max_count              = 5
+    vm_size                = "Standard_D2as_v4"
+    type                   = "VirtualMachineScaleSets"
+    vnet_subnet_id         = azurerm_subnet.this.id
+    enable_host_encryption = true
+    zones                  = null
+
+    node_labels = {
+      nodepool = "default"
+    }
+  }
+
+  network_profile {
+    network_plugin     = "kubenet"
+    service_cidr       = local.service_cidr
+    docker_bridge_cidr = local.docker_bridge_cidr
+    dns_service_ip     = local.dns_service_ip
+    pod_cidr           = local.pod_cidr
+    load_balancer_sku  = "standard"
+    outbound_type      = "userAssignedNATGateway"
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = toset([azurerm_user_assigned_identity.aks.id])
+  }
+
+  tags = local.tags
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -26,12 +26,21 @@ variable "vpc_cidr" {
   default = "10.200.0.0/20"
 }
 
+variable "aks_cidr" {
+  type    = string
+  default = "100.100.0.0/14"
+}
+
 locals {
-  tfc_org         = split("/", var.TFC_WORKSPACE_SLUG)[0]
-  tfc_workspace   = split("/", var.TFC_WORKSPACE_SLUG)[1]
-  private_subnets = [cidrsubnet(var.vpc_cidr, 2, 0), cidrsubnet(var.vpc_cidr, 2, 1), cidrsubnet(var.vpc_cidr, 2, 2)]    // 3x /26
-  public_subnets  = [cidrsubnet(var.vpc_cidr, 4, 12), cidrsubnet(var.vpc_cidr, 4, 13), cidrsubnet(var.vpc_cidr, 4, 14)] // 3x /28
-  azs             = chunklist(data.aws_availability_zones.this.names, 3)[0]                                             // returns first three availability zones in the region as a list
+  tfc_org            = split("/", var.TFC_WORKSPACE_SLUG)[0]
+  tfc_workspace      = split("/", var.TFC_WORKSPACE_SLUG)[1]
+  private_subnets    = [cidrsubnet(var.vpc_cidr, 2, 0), cidrsubnet(var.vpc_cidr, 2, 1), cidrsubnet(var.vpc_cidr, 2, 2)]    // 3x /26
+  public_subnets     = [cidrsubnet(var.vpc_cidr, 4, 12), cidrsubnet(var.vpc_cidr, 4, 13), cidrsubnet(var.vpc_cidr, 4, 14)] // 3x /28
+  azs                = chunklist(data.aws_availability_zones.this.names, 3)[0]                                             // returns first three availability zones in the region as a list
+  service_cidr       = cidrsubnet(var.aks_cidr, 2, 0)
+  docker_bridge_cidr = "${cidrhost(cidrsubnet(var.aks_cidr, 2, 1), 1)}/${split("/", cidrsubnet(var.aks_cidr, 2, 1))[1]}"
+  pod_cidr           = cidrsubnet(var.aks_cidr, 2, 2)
+  dns_service_ip     = cidrhost(local.service_cidr, 10)
 
   tags = {
     TERRAFORM     = true
