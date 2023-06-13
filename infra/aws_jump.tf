055145166f5ab75894df42aa177244f8eb68ffaa
Chris Zembower <chris.zembower@hashicorp.com>
2023-06-13 18:41:32 -0400

    interruption behaviour

--- infra/aws_jump.tf
+++ infra/aws_jump.tf
@@ -19,7 +19,8 @@ resource "aws_instance" "this" {
   instance_market_options {
     market_type = "spot"
     spot_options {
-      spot_instance_type = "persistent"
+      spot_instance_type             = "persistent"
+      instance_interruption_behavior = "stop"
     }
   }
   instance_type = "t3.nano"
