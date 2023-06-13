4541d08963113f726da7faee5f42d074bd1e3519
Chris Zembower <chris.zembower@hashicorp.com>
2023-06-13 17:15:48 -0400

    fargate profile

--- infra/aws_k8s.tf
+++ infra/aws_k8s.tf
@@ -5,18 +5,31 @@ module "eks" {
   cluster_endpoint_public_access = false
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
     kube-proxy = {
       most_recent = true
     }
     vpc-cni = {
       most_recent = true
     }
+    coredns = {
+      configuration_values = jsonencode({
+        computeType = "Fargate"
+      })
+    }
   }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
+
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "*"
+        }
+      ]
+    }
+  }
 }
