b49624c946e2e2cb61272f5125670391fc8806ea
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-03 17:21:18 -0400

    git branch var

--- k8s_app_deploy/variables.tf
+++ k8s_app_deploy/variables.tf
@@ -12,6 +12,10 @@ variable "TFC_WORKSPACE_SLUG" {
   type = string
 }
 
+variable "TFC_CONFIGURATION_VERSION_GIT_BRANCH" {
+  type = string
+}
+
 locals {
   tfc_org       = split("/", var.TFC_WORKSPACE_SLUG)[0]
   tfc_workspace = split("/", var.TFC_WORKSPACE_SLUG)[1]
