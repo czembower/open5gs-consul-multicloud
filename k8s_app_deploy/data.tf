4cf4feb4608f11543aa92d913dace703fd571a02
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-03 17:23:00 -0400

    terraform remote state data source

--- k8s_app_deploy/data.tf
+++ k8s_app_deploy/data.tf
@@ -1,3 +1,14 @@
+data "terraform_remote_state" "infra" {
+  backend = "remote"
+
+  config = {
+    hostname     = "app.terraform.io"
+    organization = local.tfc_org
+    workspaces = {
+      name = "01_infra"
+    }
+  }
+}
 data "aws_eks_cluster_auth" "this" {
   name = data.terraform_remote_state.infra.outputs.eks_cluster_name
 }
