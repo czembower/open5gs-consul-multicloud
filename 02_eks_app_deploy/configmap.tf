ae3099e78bdaa2668c02fe41669e59349b3cd7eb
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-04 15:29:34 -0400

    add fargate role arn to outputs

--- 02_eks_app_deploy/configmap.tf
+++ 02_eks_app_deploy/configmap.tf
@@ -3,6 +3,7 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
     name      = "aws-auth"
     namespace = "kube-system"
   }
+
   data = {
     "mapRoles" : "- groups:\n  - system:masters\n  rolearn: ${data.terraform_remote_state.infra.outputs.jump_iam_role}\n  username: jump\n"
   }
