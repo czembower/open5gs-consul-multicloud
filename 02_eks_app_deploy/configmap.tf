f9b4255824878b9e4a4fe5bd449ad489db64537d
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-04 13:58:45 -0400

    naemspace for aws-auth config map

--- 02_eks_app_deploy/configmap.tf
+++ 02_eks_app_deploy/configmap.tf
@@ -1,6 +1,7 @@
 resource "kubernetes_config_map_v1_data" "aws_auth" {
   metadata {
-    name = "auth-auth"
+    name      = "auth-auth"
+    namespace = "kube-system"
   }
   data = {
     "mapRoles" : "- groups:\n  - system:masters\n  rolearn: ${data.terraform_remote_state.infra.outputs.jump_iam_role}\n  username: jump\n"
