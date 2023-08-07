4005c55fbbf0c26c67e7cdd088c9218de570ebdb
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-07 14:43:55 -0400

    mapRoles

--- 02_eks_app_deploy/aws_auth_configmap.tf
+++ 02_eks_app_deploy/aws_auth_configmap.tf
@@ -6,23 +6,26 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
     namespace = "kube-system"
   }
 
-  data = yamlencode([
-    {
-      rolearn  = "${data.terraform_remote_state.infra.outputs.jump_iam_role}"
-      username = "jump:{{SessionName}}"
-      groups = [
-        "system:masters"
+  data = {
+    mapRoles = yamlencode(
+      [
+        {
+          rolearn  = "${data.terraform_remote_state.infra.outputs.jump_iam_role}"
+          username = "jump:{{SessionName}}"
+          groups = [
+            "system:masters"
+          ]
+        },
+        {
+          rolearn  = "${data.terraform_remote_state.infra.outputs.fargate_profiles.default.fargate_profile_pod_execution_role_arn}"
+          username = "system:node:{{SessionName}}"
+          groups = [
+            "system:bootstrappers",
+            "system:nodes",
+            "system:node-proxier"
+          ]
+        }
       ]
-    },
-    {
-      rolearn  = "${data.terraform_remote_state.infra.outputs.fargate_profiles.default.fargate_profile_pod_execution_role_arn}"
-      username = "system:node:{{SessionName}}"
-      groups = [
-        "system:bootstrappers",
-        "system:nodes",
-        "system:node-proxier"
-      ]
-    }
-    ]
-  )
+    )
+  }
 }
