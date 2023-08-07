01daa8495b80a8cd83b649ad06acddbbe3b48294
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-07 17:28:23 -0400

    fix config map yaml

--- 02_eks_app_deploy/aws_auth_configmap.tf
+++ 02_eks_app_deploy/aws_auth_configmap.tf
@@ -25,15 +25,15 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
         #     "system:node-proxier"
         #   ]
         # },
-        [for group in data.terraform_remote_state.infra.outputs.eks_managed_node_groups : {
-          rolearn  = group.iam_role_arn
-          username = "system:node:{{EC2PrivateDNSName}}"
-          groups = [
-            "system:bootstrappers",
-            "system:nodes",
-          ]
-          }
-        ]
+        # [for group in data.terraform_remote_state.infra.outputs.eks_managed_node_groups : {
+        #   rolearn  = group.iam_role_arn
+        #   username = "system:node:{{EC2PrivateDNSName}}"
+        #   groups = [
+        #     "system:bootstrappers",
+        #     "system:nodes",
+        #   ]
+        #   }
+        # ]
       ]
     )
   }
