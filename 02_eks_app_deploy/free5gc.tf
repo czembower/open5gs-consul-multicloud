b63a8f06095217cd9ea82805f06627edf0bc2a8b
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-07 15:14:52 -0400

    try nrf

---
+++ 02_eks_app_deploy/free5gc.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "free5gc_nrf" {
+  metadata {
+    annotations = {
+      name = "free5gc-nrf"
+    }
+    name = "free5gc-nrf"
+  }
+}
+
+
+resource "helm_release" "free5gc_nrf" {
+  name       = "towards5gs"
+  namespace  = kubernetes_namespace.free5gc_nrf.metadata[0].name
+  repository = "https://raw.githubusercontent.com/Orange-OpenSource/towards5gs-helm/main/repo/"
+  chart      = "free5gc-nrf"
+
+  # values = [
+  #   data.template_file.consul_values.rendered
+  # ]
+}
