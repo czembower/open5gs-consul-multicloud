41d65075a96c472d760ff29c4c2cc316f2059627
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-07 14:40:30 -0400

    yamlencode

--- 02_eks_app_deploy/aws_auth_configmap.tf
+++ 02_eks_app_deploy/aws_auth_configmap.tf
@@ -6,7 +6,7 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
     namespace = "kube-system"
   }
 
-  data = yamlencode(
+  data = yamlencode([
     {
       rolearn  = "${data.terraform_remote_state.infra.outputs.jump_iam_role}"
       username = "jump:{{SessionName}}"
@@ -23,5 +23,6 @@ resource "kubernetes_config_map_v1_data" "aws_auth" {
         "system:node-proxier"
       ]
     }
+    ]
   )
 }
