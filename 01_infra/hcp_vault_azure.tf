841f4b95b69896e1949fe559a4210506117b25f7
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-14 15:14:40 -0400

    dependencies

--- 01_infra/hcp_vault_azure.tf
+++ 01_infra/hcp_vault_azure.tf
@@ -1,3 +1,29 @@
+resource "azuread_service_principal" "azure_vault" {
+  application_id = hcp_azure_peering_connection.azure_vault.application_id
+}
+
+resource "azurerm_role_definition" "azure_vault" {
+  name  = "hcp-hvn-peering-access-${random_id.this.hex}"
+  scope = azurerm_virtual_network.this.id
+
+  assignable_scopes = [
+    azurerm_virtual_network.this.id
+  ]
+
+  permissions {
+    actions = [
+      "Microsoft.Network/virtualNetworks/peer/action",
+      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write"
+    ]
+  }
+}
+
+resource "azurerm_role_assignment" "azure_vault" {
+  principal_id       = azuread_service_principal.azure_vault.id
+  scope              = azurerm_virtual_network.this.id
+  role_definition_id = azurerm_role_definition.azure_vault.role_definition_resource_id
+}
+
 resource "hcp_hvn" "azure_vault" {
   hvn_id         = "hvn-azure-${random_id.this.hex}"
   cloud_provider = "azure"
@@ -28,6 +54,8 @@ resource "hcp_azure_peering_connection" "azure_vault" {
   peer_tenant_id           = data.azurerm_subscription.this.tenant_id
   peer_resource_group_name = azurerm_resource_group.this.name
   peer_vnet_region         = azurerm_virtual_network.this.location
+
+  depends_on = [azurerm_role_assignment.azure_vault]
 }
 
 data "hcp_azure_peering_connection" "azure_vault" {
@@ -41,30 +69,4 @@ resource "hcp_hvn_route" "azure_vault" {
   hvn_route_id     = "hvn-route-azure-${random_id.this.hex}"
   destination_cidr = azurerm_virtual_network.this.address_space[0]
   target_link      = data.hcp_azure_peering_connection.azure_vault.self_link
-}
-
-resource "azuread_service_principal" "azure_vault" {
-  application_id = hcp_azure_peering_connection.azure_vault.application_id
-}
-
-resource "azurerm_role_definition" "azure_vault" {
-  name  = "hcp-hvn-peering-access-${random_id.this.hex}"
-  scope = azurerm_virtual_network.this.id
-
-  assignable_scopes = [
-    azurerm_virtual_network.this.id
-  ]
-
-  permissions {
-    actions = [
-      "Microsoft.Network/virtualNetworks/peer/action",
-      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write"
-    ]
-  }
-}
-
-resource "azurerm_role_assignment" "assignment" {
-  principal_id       = azuread_service_principal.azure_vault.id
-  scope              = azurerm_virtual_network.this.id
-  role_definition_id = azurerm_role_definition.azure_vault.role_definition_resource_id
 }
