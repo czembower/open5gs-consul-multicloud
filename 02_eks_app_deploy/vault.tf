e8ab020593eadcab59fd9a4c035408f9be2edef5
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-15 12:45:48 -0400

    install VAI

--- 02_eks_app_deploy/vault.tf
+++ 02_eks_app_deploy/vault.tf
@@ -6,3 +6,30 @@ resource "kubernetes_namespace" "vault" {
     name = "vault"
   }
 }
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  namespace  = kubernetes_namespace.vault.metadata[0].name
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+
+  set {
+    name  = "server.enabled"
+    value = false
+  }
+
+  set {
+    name  = "injector.enabled"
+    value = true
+  }
+
+  set {
+    name  = "injector.replicas"
+    value = 2
+  }
+
+  set {
+    name  = "global.externalVaultAddr"
+    value = hcp_vault_cluster.aws_vault.vault_private_endpoint_url
+  }
+}
