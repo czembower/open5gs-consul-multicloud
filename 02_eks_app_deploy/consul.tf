d58e885963a1980da71b7f62a9b5ba650b57ca7a
Chris Zembower <chris.zembower@hashicorp.com>
2023-08-16 13:03:24 -0400

    extract dns name from public URL

--- 02_eks_app_deploy/consul.tf
+++ 02_eks_app_deploy/consul.tf
@@ -30,7 +30,7 @@ resource "helm_release" "consul" {
 
   set {
     name  = "externalServers.hosts"
-    value = "{${data.terraform_remote_state.infra.outputs.hcp_vault_consul.consul_public_endpoint_url}}"
+    value = "{${split("//", data.terraform_remote_state.infra.outputs.hcp_vault_consul.consul_public_endpoint_url)[1]}}"
   }
 
   set {
@@ -40,7 +40,7 @@ resource "helm_release" "consul" {
 
   set {
     name  = "client.join"
-    value = data.terraform_remote_state.infra.outputs.hcp_vault_consul.consul_public_endpoint_url
+    value = split("//", data.terraform_remote_state.infra.outputs.hcp_vault_consul.consul_public_endpoint_url)[1]
   }
 
   set {
