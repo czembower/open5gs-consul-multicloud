5ead3ba42677401c4355af92c59dd76c6f86d469
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-20 16:27:31 -0400

    remove autopilot

--- 03_eks_app_deploy/consul.tf
+++ 03_eks_app_deploy/consul.tf
@@ -1,9 +1,3 @@
-resource "consul_autopilot_config" "this" {
-  cleanup_dead_servers   = true
-  last_contact_threshold = "5s"
-  max_trailing_logs      = 500
-}
-
 resource "consul_certificate_authority" "connect_vault" {
   connect_provider = "vault"
 
