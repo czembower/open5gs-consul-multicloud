3cf23c3f3d127a1dd05a16c9cec2e6b7c535d67d
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-20 15:52:50 -0400

    install cert

--- 03_eks_app_deploy/consul.tf
+++ 03_eks_app_deploy/consul.tf
@@ -59,6 +59,26 @@ resource "helm_release" "consul" {
   }
 
   set {
+    name  = "global.tls.caCert.secretName"
+    value = kubernetes_secret.consul_ca_cert.metadata[0].name
+  }
+
+  set {
+    name  = "global.tls.caCert.secretKey"
+    value = "tls.crt"
+  }
+
+  set {
+    name  = "global.tls.caKey.secretName"
+    value = kubernetes_secret.consul_ca_cert.metadata[0].name
+  }
+
+  set {
+    name  = "global.tls.caKey.secretKey"
+    value = "tls.key"
+  }
+
+  set {
     name  = "server.enabled"
     value = false
   }
