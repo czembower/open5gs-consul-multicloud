77e7a4061a719f914865d490d2a8cf1b2e7544b7
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-20 16:54:18 -0400

    set namespace for consul ca

--- 03_eks_app_deploy/consul.tf
+++ 03_eks_app_deploy/consul.tf
@@ -2,6 +2,7 @@ resource "consul_certificate_authority" "connect_vault" {
   connect_provider = "vault"
 
   config_json = jsonencode({
+    Namespace           = "admin"
     Address             = data.terraform_remote_state.base.outputs.hcp_vault_aws.vault_public_endpoint_url
     Token               = data.terraform_remote_state.base.outputs.hcp_vault_admin_token
     RootPKIPath         = "consul-connect-root"
