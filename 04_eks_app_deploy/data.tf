0255b59f93c106ec12753f3c381dc8a9bd56cc6b
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-21 09:51:13 -0400

    fix data ref

--- 04_eks_app_deploy/data.tf
+++ 04_eks_app_deploy/data.tf
@@ -17,7 +17,7 @@ data "terraform_remote_state" "eks" {
     hostname     = "app.terraform.io"
     organization = local.tfc_org
     workspaces = {
-      name = "01_eks"
+      name = "02_eks"
     }
   }
 }
