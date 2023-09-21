c94b78732c191705d75d01af08428ad60fd7efdb
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-21 10:53:37 -0400

    create gossip key

--- 01_hcp_vault_config/kv_v2.tf
+++ 01_hcp_vault_config/kv_v2.tf
@@ -10,3 +10,18 @@ resource "vault_kv_secret_backend_v2" "this" {
   namespace = vault_namespace.consul.path
   mount     = vault_mount.kvv2.path
 }
+
+resource "random_id" "gossip_key" {
+  byte_length = 32
+}
+
+resource "vault_kv_secret_v2" "this" {
+  namespace = vault_namespace.consul.path
+  mount     = vault_mount.kvv2.path
+  name      = "gossip_key"
+  data_json = jsonencode(
+    {
+      key = "${random_id.gossip_key.b64_std}"
+    }
+  )
+}
