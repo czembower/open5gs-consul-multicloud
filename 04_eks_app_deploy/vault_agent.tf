50357758470208ebf592c14eef22b0eb5ee36800
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-21 14:16:49 -0400

    remove vso

--- 04_eks_app_deploy/vault_agent.tf
+++ 04_eks_app_deploy/vault_agent.tf
@@ -34,54 +34,54 @@ resource "helm_release" "vault" {
   }
 }
 
-resource "helm_release" "vault_secrets_operator" {
-  name       = "vault-secrets-operator"
-  namespace  = kubernetes_namespace.vault.metadata[0].name
-  repository = "https://helm.releases.hashicorp.com"
-  chart      = "vault-secrets-operator"
+# resource "helm_release" "vault_secrets_operator" {
+#   name       = "vault-secrets-operator"
+#   namespace  = kubernetes_namespace.vault.metadata[0].name
+#   repository = "https://helm.releases.hashicorp.com"
+#   chart      = "vault-secrets-operator"
 
-  set {
-    name  = "controller.replicas"
-    value = 2
-  }
+#   set {
+#     name  = "controller.replicas"
+#     value = 2
+#   }
 
-  set {
-    name  = "defaultVaultConnection.enabled"
-    value = "true"
-  }
+#   set {
+#     name  = "defaultVaultConnection.enabled"
+#     value = "true"
+#   }
 
-  set {
-    name  = "defaultVaultConnection.address"
-    value = data.terraform_remote_state.base.outputs.hcp_vault_aws.vault_public_endpoint_url
-  }
+#   set {
+#     name  = "defaultVaultConnection.address"
+#     value = data.terraform_remote_state.base.outputs.hcp_vault_aws.vault_public_endpoint_url
+#   }
 
-  set {
-    name  = "defaultAuthMethod.enabled"
-    value = true
-  }
+#   set {
+#     name  = "defaultAuthMethod.enabled"
+#     value = true
+#   }
 
-  set {
-    name  = "defaultAuthMethod.namespace"
-    value = "admin"
-  }
+#   set {
+#     name  = "defaultAuthMethod.namespace"
+#     value = "admin"
+#   }
 
-  set {
-    name  = "defaultAuthMethod.method"
-    value = "jwt"
-  }
+#   set {
+#     name  = "defaultAuthMethod.method"
+#     value = "jwt"
+#   }
 
-  set {
-    name  = "defaultAuthMethod.mount"
-    value = "jwt"
-  }
+#   set {
+#     name  = "defaultAuthMethod.mount"
+#     value = "jwt"
+#   }
 
-  set {
-    name  = "defaultAuthMethod.jwt.role"
-    value = "default"
-  }
+#   set {
+#     name  = "defaultAuthMethod.jwt.role"
+#     value = "default"
+#   }
 
-  set {
-    name  = "defaultAuthMethod.jwt.serviceAccount"
-    value = "default"
-  }
-}
+#   set {
+#     name  = "defaultAuthMethod.jwt.serviceAccount"
+#     value = "default"
+#   }
+# }
