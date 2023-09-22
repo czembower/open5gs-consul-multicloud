91788d6d0cf3ae7a732c1effdb38d03dd6272fe5
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-22 13:21:30 -0400

    debug

--- 04_eks_app_deploy/vault_auth.tf
+++ 04_eks_app_deploy/vault_auth.tf
@@ -4,8 +4,12 @@ data "tls_certificate" "eks_ca" {
   content = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_data.ca_data)
 }
 
+# data "external" "pubkey_conversion" {
+#   program = ["jq", "-n", "--arg", "pubkey", "\"$(echo \"${chomp(data.tls_certificate.eks_ca.certificates[0].cert_pem)}\" | openssl x509 -noout -pubkey | awk '{printf \"%s\\n\", $0}')\"", "'{\"public_key_pem\": $pubkey}'"]
+# }
+
 data "external" "pubkey_conversion" {
-  program = ["jq", "-n", "--arg", "pubkey", "\"$(echo \"${chomp(data.tls_certificate.eks_ca.certificates[0].cert_pem)}\" | openssl x509 -noout -pubkey | awk '{printf \"%s\\n\", $0}')\"", "'{\"public_key_pem\": $pubkey}'"]
+  program = ["jq", "-n", "--arg", "pubkey", "\"$(echo \"${chomp(data.tls_certificate.eks_ca.certificates[0].cert_pem)}\" | openssl x509 -noout -pubkey | awk '{printf \"%s\\n\", $0}')\" '{\"public_key_pem\": $pubkey}'"]
 }
 
 resource "vault_jwt_auth_backend" "this" {
