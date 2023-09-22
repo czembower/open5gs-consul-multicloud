8197450eb5c63fba64be81e7dcc56d2ea1e025e6
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-22 10:10:36 -0400

    install consul

--- 04_eks_app_deploy/consul.tf
+++ 04_eks_app_deploy/consul.tf
@@ -7,88 +7,88 @@ resource "kubernetes_namespace" "consul" {
   }
 }
 
-# resource "helm_release" "consul" {
-#   name       = "consul"
-#   namespace  = kubernetes_namespace.consul.metadata[0].name
-#   repository = "https://helm.releases.hashicorp.com"
-#   chart      = "consul"
+resource "helm_release" "consul" {
+  name       = "consul"
+  namespace  = kubernetes_namespace.consul.metadata[0].name
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "consul"
 
-#   depends_on = [
-#     vault_jwt_auth_backend_role.consul
-#   ]
+  depends_on = [
+    vault_jwt_auth_backend_role.consul
+  ]
 
-#   set {
-#     name  = "global.datacenter"
-#     value = "aws-${var.aws_region}"
-#   }
+  set {
+    name  = "global.datacenter"
+    value = "aws-${var.aws_region}"
+  }
 
-#   set {
-#     name  = "server.enabled"
-#     value = true
-#   }
+  set {
+    name  = "server.enabled"
+    value = true
+  }
 
-#   set {
-#     name  = "server.replicas"
-#     value = 3
-#   }
+  set {
+    name  = "server.replicas"
+    value = 3
+  }
 
-#   set {
-#     name  = "client.enabled"
-#     value = true
-#   }
+  set {
+    name  = "client.enabled"
+    value = true
+  }
 
-#   set {
-#     name  = "dns.enabled"
-#     value = true
-#   }
+  set {
+    name  = "dns.enabled"
+    value = true
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.enabled"
-#     value = true
-#   }
+  set {
+    name  = "global.secretsBackend.vault.enabled"
+    value = true
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.vaultNamespace"
-#     value = "admin/consul"
-#   }
+  set {
+    name  = "global.secretsBackend.vault.vaultNamespace"
+    value = "admin/consul"
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.consulServerRole"
-#     value = "consul"
-#   }
+  set {
+    name  = "global.secretsBackend.vault.consulServerRole"
+    value = "consul"
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.consulClientRole"
-#     value = "consul"
-#   }
+  set {
+    name  = "global.secretsBackend.vault.consulClientRole"
+    value = "consul"
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.manageSystemACLsRole"
-#     value = "consul"
-#   }
+  set {
+    name  = "global.secretsBackend.vault.manageSystemACLsRole"
+    value = "consul"
+  }
 
-#   set {
-#     name  = "global.secretsBackend.vault.consulCARole"
-#     value = "consul"
-#   }
+  set {
+    name  = "global.secretsBackend.vault.consulCARole"
+    value = "consul"
+  }
 
-#   set {
-#     name  = "global.tls.enabled"
-#     value = true
-#   }
+  set {
+    name  = "global.tls.enabled"
+    value = true
+  }
 
-#   set {
-#     name  = "global.tls.enableAutoEncrypt"
-#     value = true
-#   }
+  set {
+    name  = "global.tls.enableAutoEncrypt"
+    value = true
+  }
 
-#   set {
-#     name  = "global.tls.caCert.secretName"
-#     value = "consul-root/cert/ca"
-#   }
+  set {
+    name  = "global.tls.caCert.secretName"
+    value = "consul-root/cert/ca"
+  }
 
-#   set {
-#     name  = "server.serverCert.secretName"
-#     value = "consul-intermediate/issue/consul"
-#   }
-# }
+  set {
+    name  = "server.serverCert.secretName"
+    value = "consul-intermediate/issue/consul"
+  }
+}
