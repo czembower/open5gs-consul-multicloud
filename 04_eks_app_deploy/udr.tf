5be9581eea01263ae46ee5bc3f6cf7d65d0cd0af
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-24 16:43:27 -0400

    add connect proxy to udr

--- 04_eks_app_deploy/udr.tf
+++ 04_eks_app_deploy/udr.tf
@@ -26,4 +26,10 @@ resource "helm_release" "free5gc_udr" {
     name  = "global.nrf.service.name"
     value = "nrf-nnrf.${kubernetes_namespace.free5gc_nrf.metadata[0].name}.svc.cluster.local"
   }
+
+  set {
+    name  = "nrf.podAnnotations.consul\\.hashicorp\\.com/connect-inject"
+    value = "true"
+    type  = "string"
+  }
 }
