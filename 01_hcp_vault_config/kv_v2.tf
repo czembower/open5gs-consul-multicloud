3d1672f463a0252382641d58fec5606e2c1581db
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-24 12:35:17 -0400

    create acl bootstrap token

--- 01_hcp_vault_config/kv_v2.tf
+++ 01_hcp_vault_config/kv_v2.tf
@@ -15,10 +15,23 @@ resource "random_id" "gossip_key" {
   byte_length = 32
 }
 
-resource "vault_kv_secret_v2" "this" {
+resource "random_uuid" "acl_bootstrap_token" {}
+
+resource "vault_kv_secret_v2" "gossip_key" {
   namespace = vault_namespace.consul.path
   mount     = vault_mount.kvv2.path
   name      = "gossip_key"
+  data_json = jsonencode(
+    {
+      key = "${random_id.gossip_key.b64_std}"
+    }
+  )
+}
+
+resource "vault_kv_secret_v2" "acl_bootstrap_token" {
+  namespace = vault_namespace.consul.path
+  mount     = vault_mount.kvv2.path
+  name      = "acl_bootstrap_token"
   data_json = jsonencode(
     {
       key = "${random_id.gossip_key.b64_std}"
