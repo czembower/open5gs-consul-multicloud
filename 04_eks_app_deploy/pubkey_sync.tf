a3a5bce525e81a854d8864fbe10ed8d139916d9a
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-25 17:32:26 -0400

    change pubkey sync to deployment

--- 04_eks_app_deploy/pubkey_sync.tf
+++ 04_eks_app_deploy/pubkey_sync.tf
@@ -42,44 +42,44 @@ resource "kubernetes_role_binding_v1" "pubkey_sync" {
   }
 }
 
-resource "kubernetes_pod_v1" "pubkey_sync" {
+resource "kubernetes_deployment_v1" "example" {
   metadata {
     name = "pubkey-sync"
-
     labels = {
       app = "pubkey-sync"
     }
   }
 
   spec {
-    service_account_name = kubernetes_service_account_v1.pubkey_sync.metadata[0].name
-    restart_policy       = "Always"
-
-    container {
-      image             = "golang:latest"
-      name              = "golang"
-      image_pull_policy = "IfNotPresent"
-      command           = ["/bin/bash", "-c"]
-      args              = ["mkdir -p /tmp/code && cp /code/pubkey-sync.go /tmp/code/main.go && cd /tmp/code && go mod init pubkey-sync && go mod tidy && go run main.go"]
-      volume_mount {
-        name       = "code-volume"
-        mount_path = "/code"
-      }
-      liveness_probe {
-        initial_delay_seconds = 30
-        period_seconds        = 10
-        failure_threshold     = 10
-        success_threshold     = 1
-        exec {
-          command = ["stat", "/tmp/healthy"]
+    replicas = 1
+    template {
+      spec {
+        container {
+          image             = "golang:latest"
+          name              = "golang"
+          image_pull_policy = "IfNotPresent"
+          command           = ["/bin/bash", "-c"]
+          args              = ["mkdir -p /tmp/code && cp /code/pubkey-sync.go /tmp/code/main.go && cd /tmp/code && go mod init pubkey-sync && go mod tidy && go run main.go"]
+          volume_mount {
+            name       = "code-volume"
+            mount_path = "/code"
+          }
+          liveness_probe {
+            initial_delay_seconds = 30
+            period_seconds        = 10
+            failure_threshold     = 10
+            success_threshold     = 1
+            exec {
+              command = ["stat", "/tmp/healthy"]
+            }
+          }
         }
       }
-    }
-
-    volume {
-      name = "code-volume"
-      config_map {
-        name = kubernetes_config_map_v1.pubkey_sync.metadata[0].name
+      volume {
+        name = "code-volume"
+        config_map {
+          name = kubernetes_config_map_v1.pubkey_sync.metadata[0].name
+        }
       }
     }
   }
