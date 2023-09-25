8d395a18fc76efc5846e353b15a104c4f4e907ae
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-25 16:01:42 -0400

    remove token condition for k8s_patcher

--- 04_eks_app_deploy/udr.tf
+++ 04_eks_app_deploy/udr.tf
@@ -45,7 +45,6 @@ resource "null_resource" "k8s_patcher" {
   triggers = {
     endpoint = data.aws_eks_cluster.this.endpoint
     ca_crt   = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token    = data.aws_eks_cluster_auth.this.token
     release  = helm_release.free5gc_udr.metadata[0].values
   }
 
