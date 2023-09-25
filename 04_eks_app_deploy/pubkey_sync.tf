4531fe1f1615d6ae7f49bbd9c21313fe5991ded6
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-25 17:11:55 -0400

    debug

--- 04_eks_app_deploy/pubkey_sync.tf
+++ 04_eks_app_deploy/pubkey_sync.tf
@@ -67,7 +67,7 @@ resource "kubernetes_pod_v1" "pubkey_sync" {
       }
       liveness_probe {
         exec {
-          command = "stat /tmp/healthy"
+          command = ["stat", "/tmp/healthy"]
         }
       }
     }
