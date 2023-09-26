fb71ddeafd63419801a95cd7275ac57b2a94116c
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-26 12:01:40 -0400

    try vpa again

--- 04_eks_app_deploy/autoscaler.tf
+++ 04_eks_app_deploy/autoscaler.tf
@@ -4,7 +4,7 @@ resource "helm_release" "vpa" {
   name       = "vpa"
   namespace  = "kube-system"
   repository = "https://cowboysysop.github.io/charts/"
-  chart      = "cowboysysop/vertical-pod-autoscaler"
+  chart      = "vertical-pod-autoscaler"
 
   depends_on = [
     helm_release.metrics_server
