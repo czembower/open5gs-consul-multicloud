66eeee4e3cd9ebcfdd8e3e5669ad5564c80ccbcd
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-26 09:47:57 -0400

    test deploy mongo db

--- 04_eks_app_deploy/mongodb.tf
+++ 04_eks_app_deploy/mongodb.tf
@@ -20,3 +20,47 @@ resource "helm_release" "mongodb_operator" {
 
 
 }
+
+
+resource "kubernetes_manifest" "mongodb" {
+  manifest = yamldecode(<<YAML
+---
+apiVersion: mongodbcommunity.mongodb.com/v1
+kind: MongoDBCommunity
+metadata:
+  name: mongodb
+  namespace: ${kubernetes_namespace.mongodb.metadata[0].name}
+spec:
+  members: 3
+  type: ReplicaSet
+  version: "6.0.5"
+  security:
+    authentication:
+      modes: ["SCRAM"]
+  users:
+    - name: my-user
+      db: admin
+      passwordSecretRef: # a reference to the secret that will be used to generate the user's password
+        name: my-user-password
+      roles:
+        - name: clusterAdmin
+          db: admin
+        - name: userAdminAnyDatabase
+          db: admin
+      scramCredentialsSecretName: my-scram
+  additionalMongodConfig:
+    storage.wiredTiger.engineConfig.journalCompressor: zlib
+
+# the user credentials will be generated from this secret
+# once the credentials are generated, this secret is no longer required
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: my-user-password
+type: Opaque
+stringData:
+  password: <your-password-here>
+YAML
+  )
+}
