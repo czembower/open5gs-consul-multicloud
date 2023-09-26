ad9d6b0e1194deeef53af4386b38fbdded9d0c70
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-26 13:02:09 -0400

    consul/vault vpa's

--- 04_eks_app_deploy/autoscaler.tf
+++ 04_eks_app_deploy/autoscaler.tf
@@ -10,3 +10,63 @@ resource "helm_release" "vpa" {
     helm_release.metrics_server
   ]
 }
+
+resource "kubernetes_manifest" "vault_vpa" {
+  manifest = yamldecode(<<YAML
+apiVersion: "autoscaling.k8s.io/v1"
+kind: VerticalPodAutoscaler
+metadata:
+  name: vault-vpa
+  namespace: vault
+spec:
+  targetRef:
+    apiVersion: "apps/v1"
+    kind: Deployment
+    name: vault-agent-injector
+  resourcePolicy:
+    containerPolicies:
+      - containerName: '*'
+        minAllowed:
+          cpu: 100m
+          memory: 50Mi
+        maxAllowed:
+          cpu: 1
+          memory: 500Mi
+        controlledResources: ["cpu", "memory"]
+YAML
+  )
+
+  depends_on = [
+    helm_release.vault
+  ]
+}
+
+resource "kubernetes_manifest" "consul_server_vpa" {
+  manifest = yamldecode(<<YAML
+apiVersion: "autoscaling.k8s.io/v1"
+kind: VerticalPodAutoscaler
+metadata:
+  name: consul-vpa
+  namespace: consul
+spec:
+  targetRef:
+    apiVersion: "apps/v1"
+    kind: StatefulSet
+    name: consul-consul-server
+  resourcePolicy:
+    containerPolicies:
+      - containerName: '*'
+        minAllowed:
+          cpu: 100m
+          memory: 200Mi
+        maxAllowed:
+          cpu: 1
+          memory: 500Mi
+        controlledResources: ["cpu", "memory"]
+YAML
+  )
+
+  depends_on = [
+    helm_release.consul
+  ]
+}
