bbb6829f6e3f559b83ee295beb633b6f61c1a560
Chris Zembower <chris.zembower@hashicorp.com>
2023-09-26 13:20:00 -0400

    consul client and mongodb vpa's

--- 04_eks_app_deploy/autoscaler.tf
+++ 04_eks_app_deploy/autoscaler.tf
@@ -70,3 +70,63 @@ YAML
     helm_release.consul
   ]
 }
+
+resource "kubernetes_manifest" "consul_client_vpa" {
+  manifest = yamldecode(<<YAML
+apiVersion: "autoscaling.k8s.io/v1"
+kind: VerticalPodAutoscaler
+metadata:
+  name: consul-client-vpa
+  namespace: consul
+spec:
+  targetRef:
+    apiVersion: "apps/v1"
+    kind: DaemonSet
+    name: consul-consul-client
+  resourcePolicy:
+    containerPolicies:
+      - containerName: '*'
+        minAllowed:
+          cpu: 50m
+          memory: 50Mi
+        maxAllowed:
+          cpu: 100
+          memory: 100Mi
+        controlledResources: ["cpu", "memory"]
+YAML
+  )
+
+  depends_on = [
+    helm_release.consul
+  ]
+}
+
+resource "kubernetes_manifest" "mongodb_vpa" {
+  manifest = yamldecode(<<YAML
+apiVersion: "autoscaling.k8s.io/v1"
+kind: VerticalPodAutoscaler
+metadata:
+  name: mongodb-vpa
+  namespace: mongodb
+spec:
+  targetRef:
+    apiVersion: "apps/v1"
+    kind: StatefulSet
+    name: mongodb
+  resourcePolicy:
+    containerPolicies:
+      - containerName: '*'
+        minAllowed:
+          cpu: 300m
+          memory: 300Mi
+        maxAllowed:
+          cpu: 1
+          memory: 500Mi
+        controlledResources: ["cpu", "memory"]
+YAML
+  )
+
+  depends_on = [
+    kubernetes_manifest.mongodb
+  ]
+}
